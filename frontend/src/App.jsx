32e94d88338b172a64da0847c1b324dbb21ffdc0
quietjun802 <whdydwns802@gmail.com>
2025-08-13 11:40:18 +0900

    1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import PostForm from './componets/PostForm'
+
+function App() {
+
+  return (
+    <div>
+      <PostForm/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/componets/PostForm.jsx
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const PostForm = () => {
+
+    const API = import.meta.env.VITE_API_URL
+
+    const [posts,setPosts]=useState([])
+
+    const fetchPosts=async()=>{
+        try {
+
+            // console.log('hello')
+            const res = await axios.get(`${API}/api/posts`)
+            const data = Array.isArray(res.data)? res.data : res.data.posts?? []
+            // console.log(data)
+            setPosts(data)
+
+        } catch (error) {
+            console.log(error,"불러오기 실패")
+        }
+    }
+
+    fetchPosts()
+
+  return (
+        <div>
+            {posts.map((post)=>(
+                <div key={post._id}>
+                    {post.title} /
+                    {post.content} /{post.auther}
+                    </div>
+            ))}
+        </div>
+    )
+}
+
+export default PostForm
